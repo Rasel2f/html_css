5541294dfb1786382879b8360986dd5d0bfcae50
Rasel2f <30306452+Rasel2f@users.noreply.github.com>
2017-07-31 19:33:51 -0700

    c10

---
+++ c-10/assets/js/main.js
@@ -0,0 +1,11 @@
+jQuery(document).ready(function($) {
+    $(".homepage-slides").owlCarousel({
+        items: 1,
+        loop:true,
+        autoplay:true,
+        autoplayTimeout:4000,
+        nav:true,
+        navText:["<i class='fa fa-long-arrow-left'></i>", "<i class='fa fa-long-arrow-right'></i>"]
+        
+    });
+});
