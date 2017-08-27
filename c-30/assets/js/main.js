c7b646261563a72388f3eff780e33b73de7371f0
Rasel2f <30306452+Rasel2f@users.noreply.github.com>
2017-08-27 22:33:07 +0600

    c30

---
+++ c-30/assets/js/main.js
@@ -0,0 +1,23 @@
+jQuery(document).ready(function($) {
+    $(".portfolio-list").masonry({
+        
+        
+    });
+    
+    $(".single-portfolio-item").hover(function () {
+       $(".single-portfolio-item .portfolio-hover h2").removeClass("animated slideInUp"); 
+       $(this).find(".portfolio-hover h2").addClass("animated slideInUp"); 
+    });
+    
+    $(".menu-trigger").on('click', function () {
+        $(".offcanvas-menu").addClass("active");
+        $(".offcanvas-menu-overlay").addClass("active");
+    });
+    
+    $(".menu-close, .offcanvas-menu-overlay").on('click', function () {
+        $(".offcanvas-menu").removeClass("active");
+        $(".offcanvas-menu-overlay").removeClass("active");
+    });
+    
+    $(".header-area").headroom();
+});
