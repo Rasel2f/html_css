cf442b19fbc0a5dabd8ac07e3925aaf182442394
Rasel2f <30306452+Rasel2f@users.noreply.github.com>
2017-11-02 12:24:57 +0600

    d1-c9

---
+++ d1/c-9/assets/js/main.js
@@ -0,0 +1,12 @@
+jQuery(document).ready(function($) {
+    $(".client-testimonial-carousel").owlCarousel({
+        items: 3,
+        margin: 30,
+        loop:true,
+        autoplay:false,
+        autoplayTimeout:3000,
+        nav:false,
+        dots: true,
+        
+    });
+});
