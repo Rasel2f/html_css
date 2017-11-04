3c81d4e947f7b3db71aed4b874c95e2d6b8c69b6
Rasel2f <30306452+Rasel2f@users.noreply.github.com>
2017-11-04 12:55:47 +0600

    c50

---
+++ c-50/assets/js/main.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function($) {
+    $(".home-slider-area").owlCarousel({
+        items: 1,
+        loop: true,
+        nev: false,
+        dots: true,
+        autoplay: true,
+        
+    });
+});
