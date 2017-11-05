754bae4b654f667bb1ef275f905b000d089affa3
Rasel2f <30306452+Rasel2f@users.noreply.github.com>
2017-11-05 12:17:08 +0600

    d1-c11

---
+++ d1/c-11/assets/js/main.js
@@ -0,0 +1,12 @@
+jQuery(document).ready(function($) {
+    $(".team-carousel").owlCarousel({
+        items: 3,
+        margin: 30,
+        loop:true,
+        autoplay:true,
+        autoplayTimeout:3000,
+        nav:true,
+        navText:["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"]
+        
+    });
+});
