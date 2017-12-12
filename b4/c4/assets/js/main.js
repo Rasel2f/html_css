98191a605fb782c5aedf15d66e6b889d6eca638b
Rasel2f <rasel2f@gmail.com>
2017-12-12 21:07:03 +0600

    b4-c4

---
+++ b4/c4/assets/js/main.js
@@ -0,0 +1,13 @@
+jQuery(document).ready(function ($) {
+
+    //Popup Plugin js Active
+    $('.video-btn').magnificPopup({
+        type: 'video'
+    });
+
+    //Counter Up Plugin js Active
+    $('.counter').counterUp({
+        delay: 10,
+        time: 1000
+    });
+});
