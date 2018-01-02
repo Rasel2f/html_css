95ee7fc52555325f8a7aa94e5106887aa1d4d31a
Rasel2f <rasel2f@gmail.com>
2018-01-02 22:51:42 +0600

    d1-c21

---
+++ d1/c-21/assets/js/main.js
@@ -0,0 +1,11 @@
+jQuery(document).ready(function($) {
+    $(".homepage-slides").owlCarousel({
+        items: 1,
+        loop:true,
+        autoplay:true,
+        autoplayTimeout:3000,
+        nav:false,
+        dots:true,
+        
+    });
+});
