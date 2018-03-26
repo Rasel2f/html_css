097e7dd29758c796afb3030a16360c9dc2858e93
Rasel2f <rasel2f@gmail.com>
2018-03-26 12:49:04 +0600

    d1-c29

---
+++ d1/c-29/assets/js/main.js
@@ -0,0 +1,26 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+        
+        $('.projects-titles li').on('click', function() {
+            
+            $('.projects-titles li').removeClass('active');
+            $(this).addClass('active');
+            
+           var selector = $(this).attr('data-filter');
+            $('.projects-list').isotope({
+                filter: selector
+            });
+        });
+    });
+
+
+    jQuery(window).load(function(){
+        
+        jQuery('.projects-list').isotope();
+        
+    });
+
+
+}(jQuery));	
