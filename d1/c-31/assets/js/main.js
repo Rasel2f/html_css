bc47a311c9c518c266d15f0db77d4fbc24e7d276
Rasel2f <30306452+Rasel2f@users.noreply.github.com>
2018-05-14 10:00:08 +0600

    d1-c31

---
+++ d1/c-31/assets/js/main.js
@@ -0,0 +1,24 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+        var s = skrollr.init({
+            render: function(data) {
+                //Debugging - Log the current scrool position.
+                //console.log(data.curTop);
+            }
+        });
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
