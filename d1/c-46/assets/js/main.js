c5fa32781a57988028348df0d58a919473d2f17c
Rasel2f <30306452+Rasel2f@users.noreply.github.com>
2018-06-29 22:46:48 +0600

    d1-c46

---
+++ d1/c-46/assets/js/main.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function($) {
+    $(".featured-trailor-carousel").owlCarousel({
+        items: 1,
+        loop:true,
+        autoplay:true,
+        autoplayTimeout:3000,
+        nav:false,
+        dots: true,
+    });
+});
