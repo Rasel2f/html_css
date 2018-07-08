00b5e49bb0355e85999784247e052b7af18a93c7
Rasel2f <30306452+Rasel2f@users.noreply.github.com>
2018-07-08 16:01:33 +0600

    d1-c66

---
+++ d1/c-66/assets/js/main.js
@@ -0,0 +1,18 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+        $(".manimenu-area").sticky({topSpacing:0});
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
