0598a5395d8fabf85f2a0bb44ea51c9a6657cb8f
Rasel2f <30306452+Rasel2f@users.noreply.github.com>
2018-07-10 12:34:33 +0600

    d1-c71

---
+++ d1/c-71/assets/js/main.js
@@ -0,0 +1,24 @@
+(function ($) {
+    "use strict";
+
+    jQuery(document).ready(function ($) {
+
+
+        $(".embed-responsive iframe").addClass("embed-responsive-item");
+        $(".carousel-inner .item:first-child").addClass("active");
+
+        $('[data-toggle="tooltip"]').tooltip()
+        $('[data-toggle="popover"]').popover()
+
+
+
+    });
+
+
+    jQuery(window).load(function () {
+
+
+    });
+
+
+}(jQuery));
